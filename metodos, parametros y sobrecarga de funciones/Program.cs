9bb995173e48ef8e0944a38f05064eefdb03ca3f
Mauricio Mejia Estevez <mme0504@gmail.com>
2018-08-07 14:19:40 -0500

    Add files via upload

---
+++ metodos, parametros y sobrecarga de funciones/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Metodos, parametros y sobrecarga de funciones
+
+namespace metodos__parametros_y_sobrecarga_de_funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            mimetodo("Juan");
+            mimetodo("Jose");
+            mimetodo("Juana");
+            mimetodo("Jaime");
+            mimetodo();
+            mimetodo("Carlos", "40", "Ing Sistemas");
+            Console.ReadLine();
+        }
+
+        static void mimetodo()
+        {
+            Console.WriteLine("Hola, Anonimo , ¿Como estas?");
+        }
+
+        static void mimetodo(string nombre)
+        {
+            Console.WriteLine("Hola, " + nombre + " , ¿Como estas?");
+        }
+
+        static void mimetodo(string nombre, string edad, string carrera)
+        {
+            Console.WriteLine("Hola, " + nombre + " , ¿Como estas?. Su edad es " + edad + "estudias " + carrera + "");
+        }
+    }
+}
+
